4699e64b43106688cb0fbbb45a2df28d814707b6
1Bayshore <owen@luebke.us>
2018-11-01 19:02:34 -0400

    added more classes

--- turn.py
+++ turn.py
@@ -5,7 +5,7 @@ class Airline:
         self.airports = l_airports
         self.routes = l_routes
 
-class AirplaneType:
+class AirplaneModel:
     def __init__(self, num_seats, miles_per_gallon, miles_range, cruising_altitude, speed):
         self.num_seats = num_seats
         self.mpg = miles_per_gallon
@@ -14,8 +14,29 @@ class AirplaneType:
         self.speed = speed
 
 class Airplane:
-    def __init__(self, type, l_classes):
-        pass
+    def __init__(self, model: AirplaneModel, l_classes):
+        self.model: AirplaneModel = model
+        self.l_classes = l_classes
+    
+class SeatClass:
+    def __init__(self, num_seats, name, cost = 100):
+        self.cost = cost
+        self.total_seats = num_seats
+        self.name = name
 
+    def set_price(self, new_price):
+        self.cost = new_price
+
+class Airport:
+    def __init__(self, name, code, num_gates, num_citizens, lat, long):
+        self.name = name
+        self.code = code
+        self.num_gates = num_gates
+        self.citizens = num_citizens
+        self.lat = lat
+        self.long = longatude
+
+class Citizen:
+    pass
 
 
